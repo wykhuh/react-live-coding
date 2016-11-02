35cc49a2cda10b831518339fb8549d3205bf95b3
Wai-Yin Kwan <wykhuh@icloud.com>
2016-11-01 20:56:41 -0700

    add link to Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,19 @@
 var React = require('react');
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
+
 
 var Home = React.createClass({
   render: function() {
     return(
-      <div>Hello from Home</div>
+      <div className="jumbotron col-sm-12 text-center">
+      <h1>Github Battle</h1>
+      <Link to="/playerOne">
+        <button type="button" className="btn btn-lg btn-success">
+          Get Started
+        </button>
+      </Link>
+      </div>
     )
   }
 })
