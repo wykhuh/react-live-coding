857c0837875e96b967b558c8baeeed94b3051d34
Wai-Yin Kwan <wykhuh@icloud.com>
2016-11-01 20:19:02 -0700

    add Bootstrap

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ var React = require('react');
 var Main = React.createClass({
   render: function() {
     return(
-      <div>
+      <div className="main-container">
       Hello from Main
         {this.props.children}
       </div>
