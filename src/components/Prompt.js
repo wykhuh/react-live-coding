45d2bf04935027135ff0b88f80536d3602789b15
Wai-Yin Kwan <wykhuh@icloud.com>
2016-11-14 20:09:13 -0800

    make separate prompt container

---
+++ src/components/Prompt.js
@@ -0,0 +1,33 @@
+var React = require('react');
+var transparentBg = require('../styles').transparentBg;
+
+var Prompt = React.createClass({
+  render: function() {
+    return (
+      <div style={transparentBg} className="jumbotron col-sm-6 col-sm-offset-3 text-center">
+        <h1>{this.props.header}</h1>
+        <div className="col-sm-12">
+          <form onSubmit={this.props.onSubmitUser}>
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                value={this.props.username}
+                onChange={this.props.onUpdateUser}
+                placeholder="Github Username"/>
+            </div>
+            <div className="form-group col-sm-3 col-sm-offset-4">
+              <button
+                className="btn btn-block btn-success"
+                type="submit">
+                Continue
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    )
+  }
+})
+
+module.exports = Prompt;
--- src/containers/PromptContainer.js
+++ src/containers/PromptContainer.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var transparentBg = require('../styles').transparentBg;
+var Prompt = require('../components/Prompt');
 
 var PromptContainer = React.createClass({
   contextTypes: {
@@ -42,28 +43,12 @@ var PromptContainer = React.createClass({
 
   render: function() {
     return(
-      <div style={transparentBg} className="jumbotron col-sm-6 col-sm-offset-3 text-center">
-        <h1>{this.props.route.header}</h1>
-        <div className="col-sm-12">
-          <form onSubmit={this.onSubmitUser}>
-            <div className="form-group">
-              <input
-                type="text"
-                className="form-control"
-                value={this.state.username}
-                onChange={this.onUpdateUser}
-                placeholder="Github Username"/>
-            </div>
-            <div className="form-group col-sm-3 col-sm-offset-4">
-              <button
-                className="btn btn-block btn-success"
-                type="submit">
-                Continue
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
+      <Prompt
+        onSubmitUser={this.onSubmitUser}
+        onUpdateUser={this.onUpdateUser}
+        header={this.props.route.header}
+        username={this.state.username}
+      />
     )
   }
 })
