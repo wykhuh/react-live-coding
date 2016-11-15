2eabb3d4869895464be83c6967a6c528a61cd996
Wai-Yin Kwan <wykhuh@icloud.com>
2016-11-14 20:28:53 -0800

    convert to funtional component

--- src/components/Prompt.js
+++ src/components/Prompt.js
@@ -1,33 +1,31 @@
 var React = require('react');
 var transparentBg = require('../styles').transparentBg;
 
-var Prompt = React.createClass({
-  render: function() {
-    return (
-      <div style={transparentBg} className="jumbotron col-sm-6 col-sm-offset-3 text-center">
-        <h1>{this.props.header}</h1>
-        <div className="col-sm-12">
-          <form onSubmit={this.props.onSubmitUser}>
-            <div className="form-group">
-              <input
-                type="text"
-                className="form-control"
-                value={ 
-                onChange={this.props.onUpdateUser}
-                placeholder="Github Username"/>
-            </div>
-            <div className="form-group col-sm-3 col-sm-offset-4">
-              <button
-                className="btn btn-block btn-success"
-                type="submit">
-                Continue
-              </button>
-            </div>
-          </form>
-        </div>
+function Prompt(props) {
+  return (
+    <div style={transparentBg} className="jumbotron col-sm-6 col-sm-offset-3 text-center">
+      <h1>{props.header}</h1>
+      <div className="col-sm-12">
+        <form onSubmit={props.onSubmitUser}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              value={props.username}
+              onChange={props.onUpdateUser}
+              placeholder="Github Username"/>
+          </div>
+          <div className="form-group col-sm-3 col-sm-offset-4">
+            <button
+              className="btn btn-block btn-success"
+              type="submit">
+              Continue
+            </button>
+          </div>
+        </form>
       </div>
-    )
-  }
-})
+    </div>
+  )
+}
 
 module.exports = Prompt;
